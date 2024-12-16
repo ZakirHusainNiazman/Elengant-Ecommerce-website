1b8b8bdcc8df85fd99767ad717a9fa91b83cb91b
Zakir Hussain Niazman <zakirhussainniazman@gmail.com>
2024-12-16 15:23:40 +0430

    All most done all the frontend

--- src/ui/input/Input.jsx
+++ src/ui/input/Input.jsx
@@ -1,9 +1,9 @@
 import cssClasses from './Input.module.css'
 
-function Input({label,name,placeholder}) {
+function Input({label,error,type="text",isValid,...props}) {
   return (
     <div className={cssClasses["input-wrapper"]}>
-      <label className={cssClasses["label"]} htmlFor={name}>
+      <label className={cssClasses["label"]} htmlFor={props.name}>
         {label}
       </label>
       <input
